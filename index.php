a0e449077566d6b7676381ef138b3afd4d0b79a4
izza_masss <48864843+izzamasss@users.noreply.github.com>
2020-06-21 16:44:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -88,7 +88,7 @@ switch (ENVIRONMENT)
 		echo 'The application environment is not set correctly.';
 		exit(1); // EXIT_ERROR
 }
-
+//IzzaMaimunah
 /*
  *---------------------------------------------------------------
  * SYSTEM DIRECTORY NAME
